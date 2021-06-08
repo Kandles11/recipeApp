624f650fe4b988c18450089dd58134ebad5950d2
Mason Thomas <kandleseatwicks@gmail.com>
2021-06-07 21:21:33 -0500

    Initial Commit

---
+++ Shared/api_test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  api_test_appApp.swift
+//  Shared
+//
+//  Created by Mason Thomas on 6/7/21.
+//
+
+import SwiftUI
+
+@main
+struct api_test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
